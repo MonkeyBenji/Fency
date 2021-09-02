beb95da822d40e257bfcb02e730aa985eab7dc6c
Benji <a@p.ie>
2021-09-02 10:44:12 +0200

    Added add to recent job fab; Fixed FAB location change issues

--- scripts/connexys/dash-autorefresh.js
+++ scripts/connexys/dash-autorefresh.js
@@ -1,8 +1,6 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   Monkey.waitForSelector(".lastRefreshDetail .slds-icon-text-warning")
     .then(async (warning) => {
-      // const dashboard = warning.closest(".dashboard");
-      // const refresh = dashboard.querySelector("button.refresh");
       const refresh = await Monkey.waitForSelector("button.refresh:enabled");
       console.log("Refreshing dashboard", refresh);
       refresh.click();
