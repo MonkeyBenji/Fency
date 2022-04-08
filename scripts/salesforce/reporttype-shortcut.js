9491fe1674990e054741c7e542cc1a367e823b6b
MonkeyBenji <33251352+MonkeyBenji@users.noreply.github.com>
2022-04-08 13:41:26 +0200

    Report type edit link didn't work (anymore) (#26)
    Co-authored-by: Benji <a@p.ie>

--- scripts/salesforce/reporttype-shortcut.js
+++ scripts/salesforce/reporttype-shortcut.js
@@ -1,7 +1,8 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   const reportTypeName = await Monkey.waitForSelector(
-    ".reportBuilder-view li > span"
+    ".reportBuilder .panelItemGroup .dash-title"
   );
+  await Monkey.sleep(13.37);
   reportTypeName.innerHTML +=
-    ' <a href="/lightning/setup/CustomReportTypes/home" target="_top">(edit types)</a>';
+    ' <a href="/lightning/setup/CustomReportTypes/home" target="_top" style="padding-left:0.5em">(edit types)</a>';
 });
