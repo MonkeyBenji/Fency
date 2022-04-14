857d282f6928fe4ae7be697c82848b0cec94d211
MonkeyBenji <33251352+MonkeyBenji@users.noreply.github.com>
2022-04-14 14:33:50 +0200

    Ports are strings (#30)
    Co-authored-by: Benji <a@p.ie>

--- scripts/walhalla/login-token.js
+++ scripts/walhalla/login-token.js
@@ -1,6 +1,9 @@
 if (
-  window.location.port == 9000 &&
-  document.body.textContent.includes("AbstractGuard.php:66")
+  parseInt(window.location.port) === 9000 &&
+  (document.body.textContent.includes("AbstractGuard.php:66") ||
+    document.body.textContent.includes(
+      "Om gebruik te maken van deze website heeft u een @inwork.nl email adres nodig."
+    ))
 ) {
   const url = new URL(window.location);
   url.searchParams.set("login-token", "development");
