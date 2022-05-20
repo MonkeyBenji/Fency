03c06e037feb3bddac44d75b8977498481110f3e
Benji <a@p.ie>
2022-05-20 14:36:05 +0200

    Membah finetuning

--- scripts/fency/membah-form.js
+++ scripts/fency/membah-form.js
@@ -1,9 +1,5 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then((Monkey) => {
   const LIMIT = 3;
-  const load = async (url, formId) => {
-    const map = await Monkey.get(`membah-${url}`, {});
-    return map[formId] ?? [];
-  };
   const save = async (url, formId, ts, data) => {
     const map = await Monkey.get(`membah-${url}`, {});
     map[formId] = (map[formId] ?? []).filter((entry) => entry.ts !== ts);
@@ -22,9 +18,10 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then((Monkey) => {
     if (ev.type === "input" && now - lastTs < 2500) return;
     lastTs = now;
     const target = ev.target;
-    const form = target.closest("form");
+    const form = target.closest && target.closest("form");
     if (!form) return;
-    const formId = [...document.querySelectorAll("form")].indexOf(form);
+    const formId =
+      form.id || [...document.querySelectorAll("form")].indexOf(form);
     const url = window.location.href.split(/[?#]/)[0];
 
     const key = `${formId};${url}`;
@@ -33,7 +30,7 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then((Monkey) => {
     }
     const ts = timestamps[key];
 
-    save(url, formId, ts, [...new FormData(form).entries()]);
+    save(url, formId, ts, [...new FormData(form)]);
   };
   document.addEventListener("input", logForm);
   document.addEventListener("change", logForm);
