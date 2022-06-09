eda07788ed9db7cfca78dd0b0b2b586b79219091
Benji <a@p.ie>
2022-06-09 14:22:16 +0200

    Execute HTML in mails logged

--- scripts/connexys/auto-login-365.js
+++ scripts/connexys/auto-login-365.js
@@ -1,7 +1,7 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
+  if (location.pathname !== "/") return;
   const office365Button = await Monkey.waitForSelector(
     "#idp_section_buttons > button"
   );
-  console.log(office365Button);
   office365Button.click();
 });
