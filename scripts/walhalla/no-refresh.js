971154cf7051fdc878e94535c4f684057b106407
MonkeyBenji <33251352+MonkeyBenji@users.noreply.github.com>
2022-07-07 10:14:58 +0200

    Walhalla 5400 (#48)
    * Port 5400 WalHalla
    
    * Oops
    
    Co-authored-by: Benji <a@p.ie>

--- scripts/walhalla/no-refresh.js
+++ scripts/walhalla/no-refresh.js
@@ -1,5 +1,5 @@
 if (
-  [5400, 9000].includes(parseInt(url.port)) ||
+  [5400, 9000].includes(parseInt(window.location.port)) ||
   window.location.host === "walhalla.inwork.nl"
 ) {
   import(chrome.runtime.getURL("/lib/monkey-script.js")).then(
