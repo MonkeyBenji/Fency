2d7bab4f67f3dc8e29a699933d4182379dfac8a7
Benji <a@p.ie>
2022-09-28 14:20:47 +0200

    Optimized some report creation thingies; removed duplicate fab entries

--- scripts/connexys/default-report-filter-iw.js
+++ scripts/connexys/default-report-filter-iw.js
@@ -1,11 +1,19 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
-  Monkey.waitForSelector("#modal-search-input")
-    .then((input) => {
-      setTimeout(() => {
-        input.focus();
-        input.value = "[iw]";
-        Monkey.type(" ");
-      }, 500);
-    })
-    .catch(() => {});
+  Monkey.waitForSelector(".report-type-picker li:nth-of-type(2) a").then(
+    (a) => {
+      a.innerHTML =
+        'Alle&nbsp;<span style="color: #9bcb41;font-weight: bold;">InWork</span>&nbsp;rapporten';
+      a.addEventListener(
+        "click",
+        () =>
+          setTimeout(() => {
+            const input = document.querySelector("#modal-search-input");
+            input.focus();
+            input.value = "[iw]";
+            Monkey.type(" ");
+          }),
+        123
+      );
+    }
+  );
 });
