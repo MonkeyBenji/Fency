24f3ffc3d166bd2eb52564137cb8fffb96fd1fba
Benji <a@p.ie>
2022-11-16 15:24:21 +0100

    Added more test domains for test inctrl

--- scripts/inctrl/local-link.js
+++ scripts/inctrl/local-link.js
@@ -3,14 +3,28 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
     const bookmarkBar = await Monkey.waitForSelector("#bookmark-bar");
     const url = new URL(window.location);
     let label = "2local";
-    if (url.protocol === "http:") {
-      url.host = "inctrl.inwork.nl";
-      url.protocol = "https:";
-      url.port = "";
-      label = "2live";
-    } else {
-      url.host = "127.0.0.1:8080";
-      url.protocol = "http:";
+    switch (url.host) {
+      case "inctrl.inwork.nl":
+      case "mike.inwork.nl":
+        url.host = "127.0.0.1:8080";
+        url.protocol = "http:";
+        break;
+      case "127.0.0.1:8080":
+        url.host = "inctrl.inwork.nl";
+        url.protocol = "https:";
+        url.port = "";
+        label = "2live";
+        break;
+      case "inctrl-update.dev.inwork.nl":
+        url.host = "127.0.0.1:7080";
+        url.protocol = "http:";
+        break;
+      case "127.0.0.1:7080":
+        url.host = "inctrl-update.dev.inwork.nl";
+        url.protocol = "https:";
+        url.port = "";
+        label = "2update";
+        break;
     }
     bookmarkBar.insertAdjacentHTML(
       "afterend",
