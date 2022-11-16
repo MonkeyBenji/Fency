9e13f3adafee268fc639d254f606f14e2c7b7959
Benji <a@p.ie>
2022-11-16 08:23:44 +0100

    Candidate step exand too

--- scripts/connexys/step-expand.js
+++ scripts/connexys/step-expand.js
@@ -1,6 +1,10 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   document.body.addEventListener("click", async (ev) => {
-    if (!window.location.href.includes("cxsrec__cxsJob_application__c")) return;
+    if (
+      !window.location.href.includes("cxsrec__cxsJob_application__c") &&
+      !window.location.href.includes("cxsrec__cxsCandidate__c")
+    )
+      return;
     if (!ev.target.matches("a.slds-tabs_default__link")) return;
     try {
       await Monkey.waitForSelector(".slds-timeline__item_expandable button");
