ff0f84a4da09be61b62ca4d02c32f41163e07c4e
Benji <bwijchers@inwork.nl>
2023-12-01 16:09:53 +0100

    Fix for JIRA recent, new JIRA version

--- scripts/misc/jira-recent.js
+++ scripts/misc/jira-recent.js
@@ -1,64 +1,48 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   if (!window.location.href.includes("/boards/")) return;
-  const issues = await Monkey.waitForSelector(
-    'a[href$="/issues"],a[href$="/issues/"]'
+  const boardId = window.location.pathname.split("/").slice(-1)[0];
+  const activeSprintJson = await Monkey.fetchText(
+    `https://inwork.atlassian.net/rest/agile/1.0/board/${boardId}/sprint?state=active`
   );
-  const project = issues.href
-    .split("/")
-    .filter((s) => s)
-    .slice(-2)[0];
-  const url =
-    issues.href + `?jql=project %3D "${project}" ORDER BY updated DESC`;
+  const sprintId = JSON.parse(activeSprintJson).values[0].id;
+  const json = await Monkey.fetchText(
+    `https://inwork.atlassian.net/rest/api/2/search?jql=sprint %3D ${sprintId} ORDER BY updated DESC&fields=key,updated&maxResults=100`
+  );
+  let prevDate = null;
+  let age = -1;
 
-  Monkey.fetchText(url).then((html) => {
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(html, "text/html");
+  const map = {};
 
-    const map = {};
-    let age = -1;
-    let prevUpdated = null;
-
-    dom.querySelectorAll("tr[data-testid]").forEach((tr) => {
-      if (age > 5) return;
-      const key = tr.querySelector("a").textContent.trim();
-      const updatedCol =
-        tr.lastChild.previousElementSibling.previousElementSibling;
-      const updated = updatedCol.textContent.trim();
-      if (updated !== prevUpdated) {
-        age++;
-        prevUpdated = updated;
-      }
-      map[key] = [age, updated];
-    });
-
-    const glowIt = () => {
-      document
-        .querySelectorAll(
-          '.ghx-issue,.ReactVirtualized__Grid__innerScrollContainer div[id^="card-"]'
-        )
-        .forEach((div) => {
-          if (div.id && div.id.startsWith("")) {
-          } else {
-          }
-          const key =
-            div.id && div.id.startsWith("card-")
-              ? div.id.substring(5)
-              : div.querySelector("a").dataset.tooltip;
-          if (key in map) {
-            const [age, updated] = map[key];
-            const hue = 150 + age * 25;
-            const alpha = 100 - age * 20;
-            if (div.id && div.id.startsWith("card-")) {
-              div = div.querySelector(
-                'div[data-test-id="platform-card.ui.card.focus-container"]'
-              );
-            }
-            div.style.boxShadow = `-8px -8px 16px -8px hsla(${hue}, 60%, 60%, ${alpha}%) inset`;
-            div.title = `Last update: ${updated}`;
-          }
-        });
-    };
-    glowIt();
-    document.addEventListener("click", () => setTimeout(glowIt, 1337));
+  JSON.parse(json).issues.forEach((issue) => {
+    const key = issue.key;
+    const updated = issue.fields.updated;
+    const updatedDate = updated.split("T")[0];
+    if (updatedDate !== prevDate) {
+      prevDate = updatedDate;
+      age++;
+    }
+    if (age > 5) return;
+    map[key] = [age, updated];
   });
+
+  const glowIt = () => {
+    document.querySelectorAll('div[id^="card-"]').forEach((div) => {
+      const key = div.id.substring(5);
+      if (key in map) {
+        const [age, updated] = map[key];
+        const hue = 150 + age * 25;
+        const alpha = 100 - age * 20;
+        if (div.id && div.id.startsWith("card-")) {
+          div = div.querySelector('div[data-test-id="platform-card.ui.card.focus-container"]');
+        }
+        div.style.boxShadow = `-8px -8px 16px -8px hsla(${hue}, 60%, 60%, ${alpha}%) inset`;
+        div.title = `Last update: ${updated}`;
+      }
+    });
+  };
+  glowIt();
+  document.addEventListener("click", () => setTimeout(glowIt, 1337));
+  document
+    .querySelector('div[data-testid="platform-board-kit.ui.board.scroll.board-scroll"]')
+    .addEventListener("scroll", () => setTimeout(glowIt, 1337), { useCapture: true, passive: true });
 });
