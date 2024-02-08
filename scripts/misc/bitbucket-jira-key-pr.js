e1e7577b228e0c31cb54b0438e810a6e89d61b73
MonkeyBenji <33251352+MonkeyBenji@users.noreply.github.com>
2024-02-08 07:51:34 +0100

    100% of the time, it works everytime... probably (#117)
    * 100% of the time, it works everytime... probably
    
    * Do stuff on change
    
    ---------
    
    Co-authored-by: Benji <bwijchers@inwork.nl>

--- scripts/misc/bitbucket-jira-key-pr.js
+++ scripts/misc/bitbucket-jira-key-pr.js
@@ -7,17 +7,30 @@ const MonkeySetValue = (input, value) => {
 };
 
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
-  if (!window.location.pathname.includes("pull-requests/new")) return;
-  Monkey.waitForSelector("input[name=title]").then((inputTitle) => {
-    if (!window.location.pathname.includes("pull-requests/new")) return;
-    const url = new URL(window.location);
-    const src = url.searchParams.get("source");
-    const jiraKey = src.split("-").slice(0, 2).join("-");
-    console.log(inputTitle.value.includes(jiraKey), inputTitle.value, jiraKey);
-    setTimeout(() => {
-      if (!inputTitle.value.includes(jiraKey)) {
-        MonkeySetValue(inputTitle, `${jiraKey} ${inputTitle.value}`);
-      }
-    }, 123);
+  const doStuff = () => {
+    Monkey.waitForSelector("input[name=title]").then((inputTitle) => {
+      if (!window.location.pathname.includes("pull-requests/new")) return;
+      const url = new URL(window.location);
+      const src = url.searchParams.get("source");
+      const jiraKey = src.split("-").slice(0, 2).join("-");
+      if (isNaN(jiraKey.split("-")[1].trim())) return; // hotfix
+      const goFix = () => {
+        if (!inputTitle.value?.trim()) return setTimeout(goFix, 123);
+        if (!inputTitle.value.includes(jiraKey)) {
+          MonkeySetValue(inputTitle, `${jiraKey} ${inputTitle.value}`);
+        }
+      };
+      setTimeout(goFix, 123);
+    });
+  };
+  doStuff();
+  Monkey.onLocationChange(doStuff);
+  document.addEventListener("click", (ev) => {
+    if (!ev.target.matches('div[id^="react-select"]')) return;
+    setTimeout(doStuff, 123);
+  });
+  document.addEventListener("keyup", (ev) => {
+    if (ev.key !== "Enter") return;
+    setTimeout(doStuff, 123);
   });
 });
