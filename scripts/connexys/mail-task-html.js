fd97fb4245ea98b751952a0e58fed0fd37f7a8ed
Benji <bwijchers@inwork.nl>
2024-02-23 11:58:04 +0100

    Add hyperlinks to activities

--- scripts/connexys/mail-task-html.js
+++ scripts/connexys/mail-task-html.js
@@ -5,6 +5,16 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
     const header = container.innerHTML.split("Body:")[0];
     container.innerHTML = header + body;
   };
+  const enableHieperlink = (container) => {
+    if (!container.textContent.includes("https://")) return;
+    const urls = container.textContent.split(/\s+/).filter((s) => s.startsWith("https://"));
+    urls.forEach((url) => {
+      container.innerHTML = container.innerHTML.replace(
+        url,
+        `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`
+      );
+    });
+  };
 
   // Make it work for /r/Task/
   const doStuff = async () => {
@@ -21,12 +31,12 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
     "click",
     async (ev) => {
       const target = ev.target;
-      if (!target.matches("a.chevronLink") && !target.closest("a.chevronLink"))
-        return;
+      if (!target.matches("a.chevronLink") && !target.closest("a.chevronLink")) return;
       await Monkey.sleep(100);
       const wrapper = target.closest("flexipage-aura-wrapper");
       await Monkey.waitForSelector(".bodyText");
       wrapper.querySelectorAll(".bodyText").forEach(enableHtml);
+      wrapper.querySelectorAll(".bodyText").forEach(enableHieperlink);
     },
     true
   );
