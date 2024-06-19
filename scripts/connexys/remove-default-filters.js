4f7b89ed1b89466cb5dbdc54a412f513e9a9ad55
Benji <bwijchers@inwork.nl>
2024-06-19 13:38:43 +0200

    Anonimize filter for other report types than candidates

--- scripts/connexys/remove-default-filters.js
+++ scripts/connexys/remove-default-filters.js
@@ -1,5 +1,8 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
-  Monkey.waitForSelector(".query-builder-widget li.slds-tabs_default__item:not(.slds-is-active) .tab-label", 5 * 60 * 1000)
+  Monkey.waitForSelector(
+    ".query-builder-widget li.slds-tabs_default__item:not(.slds-is-active) .tab-label",
+    5 * 60 * 1000
+  )
     .then((span) => {
       const a = Monkey.createElement(
         `<a href="#nofilter" style="position: absolute;top: 26px;right: 4px;z-index: 1337;font-weight: normal;padding-top:11px;line-height:18px">#nofilter</a>`
@@ -18,7 +21,9 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
         buttonScope.click();
         const picklistScope = await Monkey.waitForSelector(".filter-widget .slds-picklist button");
         picklistScope.click();
-        const picklistItemAllOfThem = await Monkey.waitForSelector(".filter-widget .slds-dropdown__item:last-of-type a");
+        const picklistItemAllOfThem = await Monkey.waitForSelector(
+          ".filter-widget .slds-dropdown__item:last-of-type a"
+        );
         picklistItemAllOfThem.click();
         const buttonApplyScope = await Monkey.waitForSelector(".filter-widget button.filter-apply");
         buttonApplyScope.click();
@@ -27,19 +32,24 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
         // Remove Creation Date Filter
         const buttonDate = await Monkey.waitForSelector(".filterContainer.STANDARDDATE button");
         buttonDate.click();
-        const picklistDate = await Monkey.waitForSelector(".filter-widget .custom-range-picklist .slds-picklist button");
+        const picklistDate = await Monkey.waitForSelector(
+          ".filter-widget .custom-range-picklist .slds-picklist button"
+        );
         picklistDate.click();
-        const picklistItemAllTheTime = await Monkey.waitForSelector(".filter-widget .slds-dropdown__item:first-of-type a");
+        const picklistItemAllTheTime = await Monkey.waitForSelector(
+          ".filter-widget .slds-dropdown__item:first-of-type a"
+        );
         picklistItemAllTheTime.click();
         const buttonApplyDate = await Monkey.waitForSelector(".filter-widget button.filter-apply");
         buttonApplyDate.click();
         await Monkey.waitForTrue(filterApplyBeGone);
 
         // Add Anonimize filter for candidates
-        if (document.querySelector(".dash-title .dash-tag").textContent === "[iw] Kandidaten") {
+        const reportName = document.querySelector(".dash-title .dash-tag").textContent.toLocaleLowerCase();
+        if (reportName.includes("kandidaten") || reportName.includes("inschrijvingen")) {
           document.querySelector(".filters-panel .slds-combobox__input").click();
           [...document.querySelectorAll("ul.report-combobox-listbox li > span")]
-            .filter((li) => li.textContent === "Anonimiseringsstatus")[0]
+            .filter((li) => li.textContent.includes("Anonimiseringsstatus"))[0]
             .click();
           (await Monkey.waitForSelector(".filter-widget .slds-picklist button")).click();
           (await Monkey.waitForSelector(".filter-widget .slds-dropdown__item:nth-of-type(2) a")).click();
