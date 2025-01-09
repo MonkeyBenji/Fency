47a51975f96e947fd261e051835c4e206ab79eeb
Benji <bwijchers@inwork.nl>
2025-01-09 14:10:13 +0100

    Discover, develop, grow

--- scripts/connexys/logo.js
+++ scripts/connexys/logo.js
@@ -1,8 +1,16 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   try {
-    const globalHeader = await Monkey.waitForSelector(
-      ".slds-global-header__logo"
-    );
-    globalHeader.outerHTML = `<a href="/">${globalHeader.outerHTML}</a>`;
+    const globalHeader = await Monkey.waitForSelector(".slds-global-header__logo");
+    const a = document.createElement("a");
+    a.setAttribute("href", "/");
+    globalHeader.insertAdjacentElement("afterend", a);
+    a.appendChild(globalHeader);
+    Monkey.css(`.slds-global-header__logo.bigger {
+      transform: scale(1.15) translate(4px, 0);
+      transition: all 10s;
+    }`);
+    setTimeout(() => {
+      globalHeader.classList.add("bigger");
+    }, 10);
   } catch (e) {}
 });
--- scripts/salesforce/fullcopy-link.js
+++ scripts/salesforce/fullcopy-link.js
@@ -1,17 +1,12 @@
 import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
   try {
-    const logo = await Monkey.waitForSelector(
-      "#oneHeader .slds-global-header__logo"
-    );
+    const logo = await Monkey.waitForSelector("#oneHeader .slds-global-header__logo");
     const getUrl = () => {
       const url = new URL(window.location);
       if (url.host.includes("--fullcopy.sandbox")) {
         url.host = url.host.replace("--fullcopy.sandbox", "");
       } else {
-        url.host = url.host.replace(
-          ".lightning",
-          "--fullcopy.sandbox.lightning"
-        );
+        url.host = url.host.replace(".lightning", "--fullcopy.sandbox.lightning");
       }
       return url;
     };
@@ -19,10 +14,7 @@ import(chrome.runtime.getURL("/lib/monkey-script.js")).then(async (Monkey) => {
     const a = document.createElement("a");
     a.innerText = isFullCopy ? "2live" : "2copy";
     a.setAttribute("href", getUrl());
-    a.setAttribute(
-      "style",
-      "top: 19px; position: absolute; left: 222px; background: #f8f8f8"
-    );
+    a.setAttribute("style", "top: 19px; position: absolute; left: 234px; background: #f8f8f8");
     a.addEventListener("click", () => (a.href = getUrl()));
     logo.parentElement.appendChild(a);
   } catch (e) {
